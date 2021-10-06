aff4bcb1561bcfed6480d6f9bc7194555adf18fe
Colin Hartley <colin.hartley@walkfares.com>
2021-10-06 14:35:45 +0100

    Fix google gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -131,6 +131,5 @@ module.exports = {
     integrity: "sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We",
     crossorigin: "anonymous"
     }
-  ],
-  plugins: ['@docusaurus/plugin-google-gtag']
+  ]
 };
