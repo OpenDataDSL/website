7f2f684a17b918d3cf1f80da85b0d6809b6d1102
Colin Hartley <colin.hartley@walkfares.com>
2021-12-06 09:20:39 +0000

    Added IpMeta

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -148,6 +148,10 @@ module.exports = {
       },
     ],
   ],
+  scripts: [
+    'https://ipmeta.io/plugin.js',
+    './src/components/ipmeta.js'
+  ],
   stylesheets: [{
     rel: "stylesheet",
     href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css",
