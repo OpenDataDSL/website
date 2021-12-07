46abb0da60527b3e888213abf10d58552c505e2c
Colin Hartley <colin.hartley@walkfares.com>
2021-12-07 10:31:07 +0000

    Added register page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -101,6 +101,10 @@ module.exports = {
               label: 'Legal Notices',
               to: '/legal/Legal-Notices',
             },
+            {
+              label: 'Register for our NewsLetter',
+              to: '/register',
+            },
           ],
         },
       ],
