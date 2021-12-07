8840af1139ea1b2404bffc31e3c5de567e2b63b1
Colin Hartley <colin.hartley@walkfares.com>
2021-12-07 16:02:31 +0000

    Add apiKey to ipMeta

--- static/js/ipmeta.js
+++ static/js/ipmeta.js
@@ -1,4 +1,5 @@
 provideGtagPlugin({
+      apiKey: '4b2e8a03ddfa42afcb459a8f1e7e9102d7e01b4453a5dba8affc597e43b73927',
       serviceProvider: 'dimension1',
       networkDomain: 'dimension2',
       networkType: 'dimension3',
