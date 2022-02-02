ba06ed8118f54e79dcbca5768d49a6e7ea858016
OpenDataDSL <colin.hartley@opendatadsl.com>
2022-02-02 09:08:42 +0000

    Added popup signup form

---
+++ static/js/mc.js
@@ -0,0 +1,4 @@
+!function(c,h,i,m,p){
+    m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)
+}
+(document,"script","https://chimpstatic.com/mcjs-connected/js/users/94ab649e417c079356b26adc3/3d5b8944f75b6a40cbc5e97ef.js");
