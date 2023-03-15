f29f123118266b6ed0ac2629ae0a773d06f9136b
OpenDataDSL <colin.hartley@opendatadsl.com>
2023-03-15 12:28:10 +0000

    Fix build issue

--- static/js/captcha.js
+++ static/js/captcha.js
@@ -1,20 +1,24 @@
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
 var _captchaTries = 0;
 
 function recaptchaOnload() {
-    _captchaTries++;
-    if (_captchaTries > 9) {
-        return;
+    if (ExecutionEnvironment.canUseDOM) {
+        _captchaTries++;
+        if (_captchaTries > 9) {
+            return;
+        }
+        try {
+            grecaptcha.render("recaptcha", {
+                sitekey: '6LczjqUkAAAAACuHIEJpp3qDNKGqZtAV0k2aRBmr',
+                callback: function() {
+                    document.getElementById("form_submit").disabled = false;
+                }
+            });
+            return;
+        } catch (e) {}
+        window.setTimeout(recaptchaOnload, 1000);
     }
-    try {
-        grecaptcha.render("recaptcha", {
-            sitekey: '6LczjqUkAAAAACuHIEJpp3qDNKGqZtAV0k2aRBmr',
-            callback: function() {
-                document.getElementById("form_submit").disabled = false;
-            }
-        });
-        return;
-    } catch (e) {}
-    window.setTimeout(recaptchaOnload, 1000);
 }
 
 export function loadReCaptcha() {
