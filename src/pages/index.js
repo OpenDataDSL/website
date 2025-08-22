cbc3b265beac9250e8c27eadc39351df12b814ea
Colin Hartley <colin.hartley@opendatadsl.com>
2025-08-22 13:17:53 +0100

    Remove platform function from main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,25 +75,6 @@ export default function Home() {
   );
 }
 
-function Platform() {
-    return (
-    <div className={styles.heroBanner}>
-        <h2>Everything you need to manage market data</h2>
-        <h4>Click on items in the diagram for more information, screenshots and videos.</h4>
-        <img src="/img/platform.png" useMap="#platmap"></img>
-        <map name="platmap">
-          <area shape="poly" coords="0,30,255,30,130,200,220,220,220,270,25,270,25,200,100,200" href="https://doc.opendatadsl.com/docs/data/catalog" target="_blank"></area>
-          <area shape="rect" coords="25,282,110,340" href="/docs/managed_services/master-data"></area>
-          <area shape="rect" coords="264,38,456,63" href="/docs/implementation/favourites"></area>
-          <area shape="rect" coords="505,80,700,105" href="/docs/integration/automations"></area>
-          <area shape="rect" coords="737,40,948,170" href="/docs/applications/web-portal"></area>
-          <area shape="rect" coords="737,182,948,320" href="/docs/applications/excel"></area>
-          <area shape="rect" coords="737,332,948,478" href="/docs/applications/odsl"></area>
-        </map>
-    </div>
-    );
-}
-
 function Tagline() {
     return (
     <div className={styles.heroBanner}>
